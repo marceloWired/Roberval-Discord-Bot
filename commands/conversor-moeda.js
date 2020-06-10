f0a0c233a19a7b66642a6ba144843b9c8b36ebcb
MarceloWired <marcelo.s01@hotmail.com>
2020-06-10 12:38:55 -0300

    nome das moedas agora aparecem formatadas

--- commands/conversor-moeda.js
+++ commands/conversor-moeda.js
@@ -14,12 +14,16 @@ module.exports = {
         const link = `https://www.google.com/search?sxsrf=ALeKk02x7fXZPqrGjDAbBL6tv5LMKqeu5Q%3A1591196652992&ei=7LvXXsLzO7vO5OUP9rufkA8&q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&gs_lcp=CgZwc3ktYWIQAzIKCAAQgwEQRhCCAjIFCAAQsQMyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADoHCAAQgwEQQzoECAAQQzoFCAAQgwE6BAgjECc6CggAELEDEEYQggJQuLIWWKbAFmDVwBZoAHAAeACAAZkBiAHzC5IBAzUuOZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwjCmZO69eXpAhU7J7kGHfbdB_IQ4dUDCAw&uact=5`
 
         await page.goto(link);
-        const valor = await page.evaluate(() => {
-            return document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc").value
+        const resultado = await page.evaluate(() => {
+            return {
+                valor: document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc").value,
+                nomeMoedaBase: document.querySelector('.vLqKYe').innerHTML,
+                nomeMoedaFinal: document.querySelector('.MWvIVe').innerHTML,
+            }
         })
         page.close();
 
-        message.reply(`1 ${moedaBase} equivale a ${valor} ${moedaFinal}`);
+        message.reply(`1 ${resultado.nomeMoedaBase} equivale a ${resultado.valor} ${resultado.nomeMoedaFinal}`);
 
     }
 }
